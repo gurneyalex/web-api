984a18037eb594dcadd0d95116c5d05fb34ab7ba
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2024-04-19 15:07:36 +0200

    Revert "fixup! add support for oauth2 web application flow"
    This reverts commit d08ea96d2d24e728c3083998adff05c7e48642c0.

--- webservice/controllers/oauth2.py
+++ webservice/controllers/oauth2.py
@@ -1,27 +1,25 @@
 # Copyright 2024 Camptocamp SA
 # @author Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-import json
 import logging
 
 from oauthlib.oauth2.rfc6749 import errors
-from werkzeug.urls import url_encode
 
 from odoo import http
-from odoo.http import request
 
 _logger = logging.getLogger(__name__)
 
 
 class OAuth2Controller(http.Controller):
     @http.route(
-        "/webservice/<int:backend_id>/oauth2/redirect",
+        "/webservice/{backend_id}/oauth2/redirect",
         type="http",
         auth="public",
+        methods=["POST"],
         csrf=False,
     )
     def redirect(self, backend_id, **params):
-        backend = request.env["webservice.backend"].browse(backend_id).sudo()
+        backend = self.env["webservice.backend"].browse(backend_id).sudo()
         if backend.auth_type != "oauth2" or backend.oauth2_flow != "authorization_code":
             _logger.error("unexpected backed config for backend %d", backend_id)
             raise errors.MismatchingRedirectURIError()
@@ -30,34 +28,13 @@ class OAuth2Controller(http.Controller):
         if state != expected_state:
             _logger.error("unexpected state: %s", state)
             raise errors.MismatchingStateError()
-        code = params.get("code")
+        params.get("code")
         adapter = (
-            backend._get_adapter()
-        )  # we expect an adapter that support authorization_code
-        token = adapter._fetch_token_from_authorization(code)
-        backend.write(
-            {
-                "oauth2_token": json.dumps(token),
-                "oauth2_state": False,
-            }
+            backend.get_adapter()
+        )  # if expect an adapter support authorization_code
+        adapter._fetch_token_from_authorization()
+        action = self.env["ir.action"]._for_xml_id(
+            "webservice.webservice_backend_act_window"
         )
-        # after saving the token, redirect to the backend form view
-        uid = request.session.uid
-        user = request.env["res.users"].sudo().browse(uid)
-        cids = request.httprequest.cookies.get("cids", str(user.company_id.id))
-        cids = [int(cid) for cid in cids.split(",")]
-        record_action = backend.get_access_action()
-        url_params = {
-            "model": backend._name,
-            "id": backend.id,
-            "active_id": backend.id,
-            "action": record_action.get("id"),
-        }
-        view_id = backend.get_formview_id()
-        if view_id:
-            url_params["view_id"] = view_id
-
-        if cids:
-            url_params["cids"] = ",".join([str(cid) for cid in cids])
-        url = "/web?#%s" % url_encode(url_params)
-        return request.redirect(url)
+        action["res_id"] = backend_id
+        return action
